95ce85840083189dfcb838c1e385101bf047bdbe
Roshan kc <kcroshan682@gmail.com>
2021-09-07 21:51:39 +0545

    Minor changes

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+# admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUser(admin.ModelAdmin):
+    list_display = ['id','email','name','username','session_token','active','is_staff','is_superuser','created_at','updated_at']
+
+
+
